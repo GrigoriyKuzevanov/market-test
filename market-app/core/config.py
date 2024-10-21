f9053ab5c94d9852259e66875a27fbd12c79d6e3
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-10-21 16:58:22 +0500

    add database settings to config.

--- market-app/core/config.py
+++ market-app/core/config.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic import BaseModel, PostgresDsn, computed_field
+from pydantic_core import MultiHostUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class AppRunConfig(BaseModel):
@@ -11,9 +12,36 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    db_user: str
+    db_password: str
+    db_host: str
+    db_port: int
+    db_name: str
+    echo_sql: bool = False
+    echo_pool: bool = False
+    pool_size: int = 5
+    max_overflow: int = 10
+    
+    @computed_field
+    @property
+    def asyncpg_url(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+asyncpg",
+            username=self.db_user,
+            password=self.db_password,
+            host=self.db_host,
+            port=self.db_port,
+            path=self.db_name,
+        )
+
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True, case_sensitive=False)
+
+
 class Settings(BaseSettings):
     app_run: AppRunConfig = AppRunConfig()
     api_prefix: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig = DatabaseConfig()
 
 
 settings = Settings()
