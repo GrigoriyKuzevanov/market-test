a51e2b11cef38eebeba40ea91c09b033f9fb91a6
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-10-21 17:58:26 +0500

    add lifespan contextmanager with db dispose.

--- market-app/main.py
+++ market-app/main.py
@@ -1,9 +1,21 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from api import router as api_router
 from core.config import settings
+from core.models import db_connector
 from fastapi import FastAPI
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # on start
+    yield
+    # on shutdown
+    await db_connector.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router, prefix=settings.api_prefix.prefix)
 
 if __name__ == "__main__":
