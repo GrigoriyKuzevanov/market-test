755e3af7b7fd75481c35a17c6ad4808cc070c5b9
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-10-22 11:58:49 +0500

    add imports to models.init file.

--- market-app/core/models/__init__.py
+++ market-app/core/models/__init__.py
@@ -1,1 +1,11 @@
+__all__ = (
+    "db_connector",
+    "Base",
+    "Category",
+    "Product",
+)
+
 from .database import db_connector
+from .base_model import Base
+from .category import Category
+from .product import Product
