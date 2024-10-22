50d47dbfef20c8191a5d520b51f1ff25f743c3fc
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-10-22 11:09:23 +0500

    add product model.

---
+++ market-app/core/models/product.py
@@ -0,0 +1,17 @@
+from decimal import Decimal
+
+from sqlalchemy import DECIMAL, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base_model import Base
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    price: Mapped[Decimal] = mapped_column(DECIMAL(10, 2), nullable=False)
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("category.id", ondelete="CASCADE"), nullable=False
+    )
+    category: Mapped["Category"] = relationship("Category", back_populates="products")
