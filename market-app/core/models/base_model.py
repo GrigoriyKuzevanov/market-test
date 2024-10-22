17e9bc41a9951c9bff08f069e01dae139f410519
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-10-22 11:09:02 +0500

    add base model.

--- market-app/core/models/base_model.py
+++ market-app/core/models/base_model.py
@@ -1,3 +1,7 @@
+import datetime
+from datetime import datetime as dt
+
+from sqlalchemy import DateTime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -6,4 +10,10 @@ class Base(DeclarativeBase):
 
     id: Mapped[int] = mapped_column(
         primary_key=True, nullable=False, autoincrement=True
+    )
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        default=dt.now(datetime.timezone.utc)
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        onupdate=dt.now(datetime.timezone.utc)
     )
