eeaec1bbeba0f8840cb8767879b76c8673615017
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-10-23 10:51:03 +0500

    create schema for categories.

---
+++ market-app/core/schemas/category.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class CategoryBase(BaseModel):
+    name: str
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class CategoryOut(CategoryBase):
+    id: int
+
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
