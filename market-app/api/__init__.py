43e11e9d60447c18be2ee648a8000793d34137ec
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-10-23 13:42:07 +0500

    fix routers settings.

--- market-app/api/__init__.py
+++ market-app/api/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from .api_v1 import router as v1_router
+from core.config import settings
 
-router = APIRouter()
+router = APIRouter(prefix=settings.api_prefix.prefix)
 router.include_router(v1_router)
